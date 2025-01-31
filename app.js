6179cebf92e793fec133e26b7eb93e3108b75c61
Adarsh K <adarshbhatk@gmail.com>
2025-01-31 12:16:54 +0530

    Add function to fetch quiz submission data

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// Import the axios library to make API requests
+const express = require('express');
 
 const axios = require('axios');
-
-const express = require('express');
 
 const app = express();
 
@@ -17,8 +15,19 @@ app.listen(3000, () => console.log('Server running on port 3000'));
 const fetchCurrentQuizData = async () => {
     try {
         const response = await axios.get('https://www.jsonkeeper.com/b/LLQT');
-        return response.data; // Return the data from the API
+        return response.data;
     } catch (error) {
         console.error('Error fetching current quiz data:', error);
+    }
+};
+
+// Function to fetch quiz submission data from Quiz Submission Endpoint
+
+const fetchQuizSubmissionData = async () => {
+    try {
+        const response = await axios.get('https://api.jsonserve.com/rJvd7g');
+        return response.data; 
+    } catch (error) {
+        console.error('Error fetching quiz submission data:', error);
     }
 };
